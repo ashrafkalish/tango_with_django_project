a4d8a2ac086345970070151f126bd55c7a89f9ed
Ashraf Kasibante <2633731k@student.gla.ac.uk>
2022-01-28 11:53:26 +0000

    chapter4

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    context_dict= {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    
+
+def about(request):
+    context_dict={'boldmessage': 'this tutorial has been put together by Ashraf'}
+    return render(request, 'rango/about.html', context=context_dict )
+    
