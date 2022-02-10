9008cb326acd8d71e648e61f3adc393d038044c1
Ashraf Kasibante <2633731k@student.gla.ac.uk>
2022-02-10 01:11:15 +0000

    chapter10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,9 @@
+from cgitb import html
 from difflib import context_diff
 from multiprocessing import context
 import profile
 from unicodedata import category
+from urllib import request, response
 from django.shortcuts import render
 from django.http import HttpResponse
 #import the category model
@@ -17,20 +19,30 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
-    
-    category_list= Category.objects.order_by('-likes')[:5]
-    page_list= Page.objects.order_by('-views')[:5]
-    context_dict={}
-    context_dict['boldmessage'] =  'Crunchy, creamy, cookie, candy, cupcake!'
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context=context_dict)
+
+    visitor_cookie_handler(request)
     
+    response = render(request, 'rango/index.html', context=context_dict)
+    
+    
+    return response
 
 def about(request):
-    context_dict={'boldmessage': 'This tutorial has been put together by Ashraf'}
+    visitor_cookie_handler(request)
+    context_dict={'boldmessage': 'This tutorial has been put together by Ashraf',
+                  'visits':request.session['visits']
+                  }
+
+    
     return render(request, 'rango/about.html', context=context_dict )
     
 def show_category(request, category_name_slug):
@@ -161,3 +173,26 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+    
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+    
+        request.session['last_visit'] = str(datetime.now())
+    else:
+    
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
